040fb38f43bd15d9b5526c979aa8ec9dc74b602f
LindaCr <lindacrook.44@gmail.com>
2021-08-09 23:08:35 -0400

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './features/user/user-list/user-list.component';
+import { UserDetailComponent } from './features/user/user-detail/user-detail.component';
+import { UserCreateComponent } from './features/user/user-create/user-create.component';
+import { UserEditComponent } from './features/user/user-edit/user-edit.component';
+import { MenuComponent } from './core/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserCreateComponent,
+    UserEditComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
