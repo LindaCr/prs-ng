4ef839a6679c2c1f0cf941427471f3708e1d6abe
LindaCr <lindacrook.44@gmail.com>
2021-08-11 21:35:54 -0400

    added code

--- src/app/features/user/user-list/user-list.component.ts
+++ src/app/features/user/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/model/user.class';
+import { SystemService } from 'src/app/service/system.service';
 import { UserService } from 'src/app/service/user.service';
 
 @Component({
@@ -13,10 +14,12 @@ export class UserListComponent implements OnInit {
   users: User[]= [];
 
   constructor(
-    private userSvc: UserService
+    private userSvc: UserService,
+    private sysSvc: SystemService
   ) { }
 
   ngOnInit(): void {
+    console.log('User logged in is: ',this.sysSvc.loggedInUser )
     this.userSvc.list()
     .subscribe(
       resp => {
