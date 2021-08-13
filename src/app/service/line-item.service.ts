6931a4d8612e34e30aa74efb26d84802295377ff
LindaCr <lindacrook.44@gmail.com>
2021-08-13 16:16:19 -0400

    updated function

--- src/app/service/line-item.service.ts
+++ src/app/service/line-item.service.ts
@@ -33,4 +33,8 @@ export class LineItemService {
   delete(id: number): Observable<LineItem> {
     return this.http.delete(URL+'/'+id) as Observable<LineItem>;
   }
+
+  getLinesForRequest(requestId: number): Observable<LineItem[]> {
+    return this.http.get(URL+'/lines-for-pr/'+requestId) as Observable<LineItem[]>;
+  }
 }
