0e9f4f2518234817c03a6ddfab7d97f130d96e84
LindaCr <lindacrook.44@gmail.com>
2021-08-17 16:07:46 -0400

    added login feature

--- src/app/features/vendor/vendor-list/vendor-list.component.ts
+++ src/app/features/vendor/vendor-list/vendor-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user.class';
 import { Vendor } from 'src/app/model/vendor.class';
+import { SystemService } from 'src/app/service/system.service';
 import { VendorService } from 'src/app/service/vendor.service';
 
 @Component({
@@ -11,12 +13,19 @@ export class VendorListComponent implements OnInit {
 
   title: string= 'Vendor-List';
   vendors: Vendor[]= [];
+  loggedInUser: User= new User();
   
   constructor(
-    private vendorSvc: VendorService
+    private vendorSvc: VendorService,
+    private sysSvc: SystemService
   ) { }
 
   ngOnInit(): void {
+
+    this.sysSvc.checkLogin();
+
+    this.loggedInUser=this.sysSvc.loggedInUser;
+
     this.vendorSvc.list()
     .subscribe(
       resp => {
